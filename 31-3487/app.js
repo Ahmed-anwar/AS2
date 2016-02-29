8f0e586d1f6fdecdbfbee1070b1b7fd5e300762a
Ahmed-anwar <ahmedanwarm@gmail.com>
2016-02-29 22:47:49 +0200

    First commit

---
+++ 31-3487/app.js
@@ -0,0 +1,42 @@
+var express = require('express')
+var app = express();
+var db = require('./db.js')
+var quotes = require('./quotes.js');
+var index = __dirname +'/public/index.html';
+app.get('/', function (req, res) {
+		res.status = 200;
+		res.sendFile(__dirname + '/public/index.html');
+		
+});
+
+
+app.get('/api/quote',function(req, res){
+	quotes.getQuoteFromDB(function(err, quote){
+		res.json(quote);
+	});
+});
+
+
+
+db.connect(function(){
+	
+});
+
+quotes.seed(function(err, seeded){
+	if(seeded) console.log("seeded")
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
---
+++ 31-3487/public/js/main.js
@@ -0,0 +1,33 @@
+var quotes = require('../../db.js')
+
+$(document).ready(function(){
+
+  // Animate "Click something."
+
+  $(document).on("click", function() {
+
+  	
+    $(document).css({"background-color": quotes.getElementByIndexElseRandom(colours)});
+      // Update quote
+      var randomQuote = quotes.getQuoteFromJSON();
+      $("h1").text('“' + randomQuote.text + '”');
+      $("h2").text(randomQuote.author);
+
+      // Animate author name
+      $('h2').typeIt();
+
+    });
+
+  	$.getJSON( "api/quote", function( data ) {
+		var text = data.text;
+		var author = data.author;
+		$('#quote').html(text);
+		$('#author').html(author);
+	});
+
+});
+
+
+var quotesArray = require('../../../quotes.json')
+// Colours array
+var colours = ["#789dd0","#a2b3cc","#9da1b6","#7f7784","#a69190","#bf7fbf","#a3d468","#9aacb0","##294d67","#88d2f6","#5df977","#b4fb38","#ffaaaa"];
